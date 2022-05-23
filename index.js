2f5e33eef838bfc45f250eb43bb40bd0c9b6737a
akila-niasa <akilaniasa@gmail.com>
2022-05-23 15:02:40 +0600

    order save

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 // l76L8p5d51Iu7g4N
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9asok.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 // client.connect(err => {
@@ -25,7 +25,9 @@ async function run(){
     try{
         await client.connect()
         const bicycleCollection = client.db("bicycleStore").collection("services")
+        const orderCollection = client.db("bicycleStore").collection("orders")
 
+         //(GET) all product 
         app.get('/service',async(req,res)=>{
       
             const data=req.query
@@ -34,6 +36,19 @@ async function run(){
             // console.log(result);
             res.send(result)
         })
+        //(GET) single product 
+    app.get("/service/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await bicycleCollection.findOne(query);
+        res.send(result);
+      })
+      //(POST) Order save
+    app.post("/saveorder", async (req, res) => {
+        order = req.body;
+        const result = await orderCollection.insertOne(order);
+        res.json(result);
+      });
     }
     finally{
 
