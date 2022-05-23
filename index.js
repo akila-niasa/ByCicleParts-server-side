214db1a740c2ed7f7c6bba84e022bde29404b53f
akila-niasa <akilaniasa@gmail.com>
2022-05-23 19:47:04 +0600

    jwt successfully added

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express=require('express')
+const express = require('express')
 require('dotenv').config()
-const app=express()
-const cors=require('cors')
+const app = express()
+const cors = require('cors')
+var jwt = require('jsonwebtoken');
 
-const port=process.env.PORT||5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
 
-// db_user
-// l76L8p5d51Iu7g4N
+
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -21,43 +21,111 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //   client.close();
 // });
 
-async function run(){
-    try{
+function verifyJWT(req,res,next){
+    const authorization=req.headers.authorization
+    // console.log(authorization);
+    if(!authorization){
+        return res.status(401).send({message:"Unauthorized access"})
+    }
+    const token=authorization.split(' ')[1]
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(err){
+            return res.status(403).send({message:"Not allow to access"})
+        }
+        req.decoded=decoded
+        next()
+    })
+}
+
+async function run() {
+    try {
         await client.connect()
         const bicycleCollection = client.db("bicycleStore").collection("services")
         const orderCollection = client.db("bicycleStore").collection("orders")
+        const userCollection = client.db("bicycleStore").collection("users")
 
-         //(GET) all product 
-        app.get('/service',async(req,res)=>{
-      
-            const data=req.query
+         //user PUT for google
+         app.put('/user/:email',async(req,res)=>{
+            const email=req.params.email
+            const user=req.body
+            const filter={email:email}
+            const options = { upsert: true };
+            const updateDoc = {
+                $set:user,
+              };
+              const result = await userCollection.updateOne(filter, updateDoc, options);
+
+              const token= jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            //   console.log(result);
+              res.send({result,accessToken:token})
+        })
+
+        //(GET) all product 
+        app.get('/service', async (req, res) => {
+
+            const data = req.query;
             const cursor = bicycleCollection.find(data);
-            const result=await cursor.toArray()
+            const result = await cursor.toArray();
             // console.log(result);
+            res.send(result);
+        });
+        //(GET) single product 
+        app.get("/service/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bicycleCollection.findOne(query);
+            res.send(result);
+        })
+
+        //(POST) Order save
+        app.post("/saveorder", async (req, res) => {
+            order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.json(result);
+        });
+
+        //(GET) Single user Orders
+        app.get("/orders",verifyJWT, async (req, res) => {
+            const email=req.query.email
+            const decodedEmail=req.decoded.email
+            if(email===decodedEmail){
+                const query = { client: email };
+            const cursor = orderCollection.find(query);
+            const result = await cursor.toArray();
+            console.log(result);
+            return res.json(result);
+            }else{
+
+                return res.status(403).send({message:"Not allow to access"})
+            }
+            
+        });
+
+        //(DELETE) DELETE Order
+        app.delete("/deleteorder/:id", async (req, res) => {
+            const productId = req.params.id;
+            // console.log(productId);
+            const query = { _id: ObjectId(productId) };
+            const result = await orderCollection.deleteOne(query);
+            res.json(result);
+        });
+
+        // (GET) get order for payment
+        app.get('/booking/:id',verifyJWT,async(req,res)=>{
+            const id =req.params.id
+            const query={_id:ObjectId(id)}
+            const result=await bookingCollection.findOne(query);
             res.send(result)
         })
-        //(GET) single product 
-    app.get("/service/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const result = await bicycleCollection.findOne(query);
-        res.send(result);
-      })
-      //(POST) Order save
-    app.post("/saveorder", async (req, res) => {
-        order = req.body;
-        const result = await orderCollection.insertOne(order);
-        res.json(result);
-      });
     }
-    finally{
+    finally {
 
     }
 }
 run().catch(console.dir)
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Welcome to Side')
 })
-app.listen(port,()=>{console.log('CURD is running',port)})
+app.listen(port, () => { console.log('CURD is running', port) })
