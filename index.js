970e0202dd3e600fd2172cc44478622796948082
akila-niasa <akilaniasa@gmail.com>
2022-05-26 04:04:02 +0600

    update profile updated

--- index.js
+++ index.js
@@ -178,16 +178,16 @@ async function run() {
 
         
         //   (PUT)Put For Update or save profile
-        app.put('/user/:email', async(req, res) =>{
+        app.put('/userprofile/:email', async(req, res) =>{
             const email = req.params.email;
             const user = req.body;
             const filter = { email: email };
-            const options = { upsert: true };
+            // const options = { upsert: true };
             const updatedDoc = {
               $set:
                user
             }
-            const updatedUser = await userCollection.updateOne(filter, updatedDoc,options);
+            const updatedUser = await userCollection.updateOne(filter, updatedDoc);
             res.send(updatedUser);
           })
       
